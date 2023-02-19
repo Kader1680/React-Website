ff78f67f05edaf20513b0258c6e5fca058e60e2d
Kader1680 <ouldhenniabaghdad@gmail.com>
2023-02-19 20:18:23 +0100

    add home page

--- src/home/home.js
+++ src/home/home.js
@@ -1,7 +1,10 @@
 import React from "react";
 import './home.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import apch from "../img/apch.png"
+import aws from "../img/aws.png"
+import icon from "../img/icon.png"
+import reqbin from "../img/reqbin.png"
 function Home() {
     return(
         <div className="ppp">
@@ -13,51 +16,55 @@ function Home() {
             
             <div class="courses row">
                 <div class="Feild col-lg-3">
-                    <h4>Crash Courses HTML & CSS</h4>
                     <div class="vedio">
                         <iframe src="https://www.youtube.com/embed/iG2jotQo9NI"></iframe>
                     </div>
+                    <h4>Crash Courses HTML & CSS</h4>
+
                     <div class="commit">
                         <i class="fa-regular fa-thumbs-up"></i>
                         <i class="fa-regular fa-thumbs-down ms-3"></i>
+                        <i class="fa-solid fa-download"></i>
                     
-                        <button class="download">Download vedio</button>
                     </div>
                 </div>
                 <div class="Feild col-lg-3">
-                    <h4>Crash Courses JavaScript</h4>
                     <div class="vedio">
                         <iframe src="https://www.youtube.com/embed/W6NZfCO5SIk&t=1290s"></iframe>
                     </div>
+                    <h4>Crash Courses JavaScript</h4>
+
                     <div class="commit">
                         <i class="fa-regular fa-thumbs-up"></i>
                         <i class="fa-regular fa-thumbs-down ms-3"></i>
+                        <i class="fa-solid fa-download"></i>
                     
-                        <button class="download">Download vedio</button>
                     </div>
                 </div>
                 <div class="Feild col-lg-3">
-                    <h4>Crash Courses SCSS</h4>
                     <div class="vedio">
                         <iframe src="https://www.youtube.com/embed/qZXt1Aom3Cs"></iframe>
                     </div>
+                    <h4>Crash Courses SCSS</h4>
+
                     <div class="commit">
                         <i class=" fa-regular fa-thumbs-up"></i>
                         <i class="fa-regular fa-thumbs-down ms-3"></i>
+                        <i class="fa-solid fa-download"></i>
                     
-                        <button class="download">Download vedio</button>
                     </div>
                 </div>
                 <div class="Feild col-lg-3">
-                    <h4>Crash Courses PHP</h4>
                     <div class="vedio">
                         <iframe src="https://www.youtube.com/embed/pszZMzI9a7A&t=3s"></iframe>
                     </div>
+                    <h4>Crash Courses PHP</h4>
+
                     <div class="commit">
                         <i class="fa-regular fa-thumbs-up"></i>
                         <i class="fa-regular fa-thumbs-down ms-3"></i>
                     
-                        <button class="download">Download vedio</button>
+                        <i class="fa-solid fa-download"></i>
                     </div>
                 </div>
             </div>
@@ -70,6 +77,76 @@ function Home() {
                 <p class='title'>Tools</p>
                 <hr></hr>
             </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            <div class="courses row">
+                <div class="Feild col-lg-3">
+                    <img src={apch} />
+                    <h4>font aswomn</h4>
+
+                    <div class="commit">
+                        <i class="fa-regular fa-thumbs-up"></i>
+                        <i class="fa-regular fa-thumbs-down ms-3"></i>
+                        <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                    
+                    </div>
+                </div>
+                <div class="Feild col-lg-3">
+                    <img src={icon} />
+                    <h4>Crash Courses JavaScript</h4>
+                    <div class="commit">
+                        <i class="fa-regular fa-thumbs-up"></i>
+                        <i class="fa-regular fa-thumbs-down ms-3"></i>
+                        <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                    </div>
+                </div>
+                <div class="Feild col-lg-3">
+                    
+                    <img src={reqbin} />
+                    <h4>Crash Courses SCSS</h4>
+                    <div class="commit">
+                        <i class=" fa-regular fa-thumbs-up"></i>
+                        <i class="fa-regular fa-thumbs-down ms-3"></i>
+                        <i class="fa-solid fa-arrow-up-right-from-square"></i>
+
+                    
+                    </div>
+                </div>
+                <div class="Feild col-lg-3">
+                    
+                    <img src={aws} />
+                    <h4>Crash Courses PHP</h4>
+                    <div class="commit">
+                        <i class="fa-regular fa-thumbs-up"></i>
+                        <i class="fa-regular fa-thumbs-down ms-3"></i>
+                        <i class="fa-solid fa-arrow-up-right-from-square"></i>
+
+                    
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
             <div class="limit d-flex align-text-center m-auto">
                 <hr></hr>
                 <p class='title'>Articles</p>
