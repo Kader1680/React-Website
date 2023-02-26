a79f2a489d6773052a1c3a4d3d0310cff43cccb5
Kader1680 <ouldhenniabaghdad@gmail.com>
2023-02-26 19:12:31 +0100

    code sign

--- src/signin/signin.js
+++ src/signin/signin.js
@@ -1,33 +1,23 @@
 import React, {useState} from "react"
 import "./sign_in.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function SignIn() {
 
     const [login, setlogin] = useState(false);
     const [sign, nonsign] = useState(true);
     function Sign() {
-        // setlogin(false)
-        // nonsign(false)
-        // setlogin(true)
         nonsign(true)
         
     }
     function Loign() {
         setlogin(true)
         nonsign(false)
-
-        // if(login == false){
-        //     setlogin(true)
-            
-        // }
-        // }else{
-        //     setlogin(false)
-        // }
     }
     return(
         <div class="myform">
             <div class ="choise">
-                <ul class=" list-unstyled">
+                <ul class=" ">
                     <li onClick={Sign}>Sign Up </li>
                     <li onClick={Loign}>Login</li>
                 </ul>
@@ -35,7 +25,7 @@ function SignIn() {
             
             <form class={ sign ? 'Sign_Up' : "SignNon" }>
                 <h6>Sign Up for free</h6>
-                <div>
+                <div class="personel">
                     <input placeholder="name"></input>
                     <input placeholder="last name"></input>
                 </div>
