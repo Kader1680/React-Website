208d32f3d9e7cfc9f67d76e2020a9fd0d522692a
Kader1680 <ouldhenniabaghdad@gmail.com>
2023-03-01 17:48:02 +0100

    add item courses

--- src/Data/coursesData.js
+++ src/Data/coursesData.js
@@ -1,14 +1,12 @@
 import htmlcss from "../img/html.png"
-// import htmlcss from "../img/html.png"
-// import htmlcss from "../img/html.png"
-// import htmlcss from "../img/html.png"
-// import htmlcss from "../img/html.png"
-// import htmlcss from "../img/html.png"
-// import htmlcss from "../img/html.png"
-// import htmlcss from "../img/html.png"
-// import htmlcss from "../img/html.png"
-// import htmlcss from "../img/html.png"
-import js from "../img/js.png"
+import css3 from "../frontend/img/css3.png"
+import bootstrap from "../frontend/img/bootstrap.png"
+import ecommerce from "../frontend/img/ecommerce.png"
+import git from "../frontend/img/git.png"
+import Gulpjs from "../frontend/img/Gulpjs.png"
+import scss from "../frontend/img/scss.png"
+import vuejs from "../frontend/img/vuejs.png"
+import js from "../frontend/img/Javascript.jpg"
 import angular from "../img/angular.png"
 export  const frontcourses = [
 
@@ -22,12 +20,54 @@ export  const frontcourses = [
     {
         id: 2,
         name: "courses jaavscripts",
-        img: js,
+        img: ecommerce,
 
+    },
+    
+    {
+        id: 1,
+        name: "learn css3 full vedio",
+        img: css3,
+    }
+    ,
+    {
+        id: 1,
+        name: "learn css3 full vedio",
+        img: vuejs,
+    }
+    ,
+    {
+        id: 1,
+        name: "learn css3 full vedio",
+        img: git,
+    }
+    ,
+    {
+        id: 1,
+        name: "learn css3 full vedio",
+        img: Gulpjs,
+    }
+    ,
+    {
+        id: 1,
+        name: "learn css3 full vedio",
+        img: js,
+    }
+    ,
+    {
+        id: 1,
+        name: "learn css3 full vedio",
+        img: scss,
     },
     {
         id: 1,
         name: "learn css3 full vedio",
         img: angular,
+    }
+    ,
+    {
+        id: 1,
+        name: "learn css3 full vedio",
+        img: bootstrap,
     }
 ]
