99c7e20867d9308b3a3803e6ac4edc180f5668ca
Kader1680 <ouldhenniabaghdad@gmail.com>
2023-03-29 23:51:46 +0100

    like state

--- src/home/homedata.jsx
+++ src/home/homedata.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './home.css'
 export default function HomeData(props) {
     const {id, title, img }= props.data
+
+    const [color, setcolor] = useState(false);
+    const Color = () =>{
+      setcolor(!color)
+    }
   return (
     
     <div class="Feild col-lg-3">
         <img src={img} />
         <a href="https://www.youtube.com/watch?v=AAu8bjj6-UI">{title}</a>
         <div class="commit">
-            <span class="material-symbols-outlined">thumb_up</span>
+            {/* <i class="fa-solid fa-thumbs-up" onClick={Color}></i> */}
+            <i class={`fa-solid fa-thumbs-up ${(color === true ? "green" : "")}`} onClick={Color}></i>
             <span class="material-symbols-outlined">download</span>
         </div>
+        
     </div>
   )
 }
